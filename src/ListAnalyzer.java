7b8aa7d6d140a55061a6e5ed5f08a001e21e573e
Javier Spina <jaspina@estudiantes.unsam.edu.ar>
2024-05-11 16:48:00 -0300

    implementacion ListAnylizer

---
+++ src/ListAnalyzer.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ListAnalyzer  {
+
+    public static int contadorImpares(List<Integer> inputList) {
+        int count = 0;
+        for(int i=0; i < inputList.size(); i++) {
+            if (inputList.get(i) % 2 == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static List<Integer> numerosConsecutivos(List<Integer> inputList) {
+        Map<Integer, Integer> consec = new HashMap<Integer, Integer>();
+        for(int i=0; i < inputList.size()-1; i++) {
+            if (inputList.get(i+1) - inputList.get(i) == 1) {
+                consec.put(i, inputList.get(i));
+                consec.put(i+1, inputList.get(i+1));
+            }
+        }
+        return List.copyOf(consec.values());
+    }
+}
